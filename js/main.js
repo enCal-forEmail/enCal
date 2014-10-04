a895c76dffe807babc65924c75ae764777745281
Andrew Millman <andrewmillman35@gmail.com>
2014-10-04 05:39:42 -0700

    Remove console.log

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready( function() {
 
   $(".tab").on("click", function() {
     var contentType = $(this).data("type");
-    console.log(contentType);
     var selectedContent = $(".tab-content[data-type='" + contentType + "']");
     $(".tab, .tab-content").removeClass("active");
     $(this).addClass("active");
