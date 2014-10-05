bd792e6af2e849d1f1c2217a27e2e4d3e395957d
Andrew Millman <andrewmillman35@gmail.com>
2014-10-05 08:08:46 -0700

    Acquire user's email after authentication

--- js/background.js
+++ js/background.js
@@ -1,18 +1,68 @@
-var i = 0;
+
+var notifs = 0;
+var BASE_URL = "http://localhost:3000"
+
+var user_email;
+var current_token;
+
+
 window.setInterval(function() {
-
-  chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-    if (chrome.runtime.lastError) {
-      console.log(chrome.runtime.lastError);
-      changeState(STATE_START);
-    } else {
-      console.log('Token acquired:'+token+
-        '. See chrome://identity-internals for details.');
-      //changeState(STATE_AUTHTOKEN_ACQUIRED);
-    }
-  });
-
-  chrome.browserAction.setBadgeText({text:String(i)});
+  chrome.browserAction.setBadgeText({text:String(notifs)});
   chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-  i++;
+  notifs++;
 }, 5000);
+
+if (current_token) {
+  chrome.identity.removeCachedAuthToken({ token: current_token }, function(){});
+
+  // Make a request to revoke token in the server
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=' +
+           current_token);
+  xhr.send();
+
+  current_token = 0;
+  console.log("Revoked token.")
+}
+
+chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+  if (chrome.runtime.lastError) {
+    console.log(chrome.runtime.lastError);
+    changeState(STATE_START);
+  } else {
+    current_token = token;
+    console.log('Token acquired:' + current_token +
+      '. See chrome://identity-internals for details.');
+
+    //GET the user's email
+    $.ajax({
+      method: 'GET',
+      url: "https://www.googleapis.com/oauth2/v1/userinfo",
+      data: {
+        "alt": "json",
+        "access_token": current_token
+      },
+      success: function(data) {
+        user_email = data.email;
+      }
+    });
+
+/*
+    $.post(
+      BASE_URL + "/login",
+      {
+        email:,
+        accessToken: current_token
+      },
+    );
+*/
+
+    changeState(STATE_AUTHTOKEN_ACQUIRED);
+  }
+});
+
+
+
+
+
+
