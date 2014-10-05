70ecf293cc8887ed8067b3004f1071a16320ac21
Andrew Millman <andrewmillman35@gmail.com>
2014-10-05 06:13:23 -0700

    Move authorization to background.js

--- js/background.js
+++ js/background.js
@@ -1,5 +1,17 @@
 var i = 0;
 window.setInterval(function() {
+
+  chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+    if (chrome.runtime.lastError) {
+      console.log(chrome.runtime.lastError);
+      changeState(STATE_START);
+    } else {
+      console.log('Token acquired:'+token+
+        '. See chrome://identity-internals for details.');
+      //changeState(STATE_AUTHTOKEN_ACQUIRED);
+    }
+  });
+
   chrome.browserAction.setBadgeText({text:String(i)});
   chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
   i++;
